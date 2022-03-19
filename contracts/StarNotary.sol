ffb6fbb7ece537a8f854217e16dd61fa67faf5f1
mujeex <mujahid.bappai@yahoo.com>
2022-03-19 21:02:55 +0100

    feat: Implement task1

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -14,6 +14,9 @@ contract StarNotary is ERC721 {
     // Implement Task 1 Add a name and symbol properties
     // name: Is a short name to your token
     // symbol: Is a short string like 'USD' -> 'American Dollar'
+    constructor() public ERC721("Beskar","BSK"){
+
+    }
     
 
     // mapping the Star with the Owner Address
@@ -57,11 +60,19 @@ contract StarNotary is ERC721 {
     // Implement Task 1 lookUptokenIdToStarInfo
     function lookUptokenIdToStarInfo (uint _tokenId) public view returns (string memory) {
         //1. You should return the Star saved in tokenIdToStarInfo mapping
+        return tokenIdToStarInfo[_tokenId];
     }
 
     // Implement Task 1 Exchange Stars function
     function exchangeStars(uint256 _tokenId1, uint256 _tokenId2) public {
         //1. Passing to star tokenId you will need to check if the owner of _tokenId1 or _tokenId2 is the sender
+        ownerOfToken1 = ownerOf(_tokenId1);
+        ownerOfToken2 = ownerOf(_tokenId2);
+        if(msg.sender == ownerOfToken1){
+            transferFrom(ownerOfToken1, ownerOfToken2, _tokenId1);
+        }else{
+            transferFrom(ownerOfToken2, ownerOfToken1, _tokenId2);
+        }
         //2. You don't have to check for the price of the token (star)
         //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId2)
         //4. Use _transferFrom function to exchange the tokens.
@@ -70,7 +81,10 @@ contract StarNotary is ERC721 {
     // Implement Task 1 Transfer Stars
     function transferStar(address _to1, uint256 _tokenId) public {
         //1. Check if the sender is the ownerOf(_tokenId)
+        require(msg.sender == ownerOf(_tokenId),"You cant transfer the Star you don't own");
+
         //2. Use the transferFrom(from, to, tokenId); function to transfer the Star
+        transferFrom(msg.sender, _to1, _tokenId);
     }
 
 }
